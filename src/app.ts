533facbd8b2a8b798165e5a6b76a8a97cfefb29d
Thomas Goillot <thomadgllt@gmail.com>
2024-11-26 12:15:23 +0100

    fix(docker): no restart need anymore

--- src/app.ts
+++ src/app.ts
@@ -9,26 +9,50 @@ import userRoutes from "./routes/userRoutes";
 const app = express();
 app.use(express.json());
 
+const MAX_RETRIES = 5; 
+const RETRY_DELAY = 5000;
 
-sequelize
-  .sync()
-  .then(() => console.log("Database connected"))
-  .catch((err: { parent: any; }) => {
-    console.error("Database connection error:", err)
-    if (err.parent) {
-      console.error("MySQL error:", err.parent)
+async function connectToDatabase() {
+  let attempt = 0;
+
+  while (attempt < MAX_RETRIES) {
+    try {
+      attempt++;
+      console.log(`Attempt ${attempt} to connect to the database...`);
+      await sequelize.authenticate(); 
+      await sequelize.sync(); 
+      console.log("Database connected successfully.");
+      return; 
+    } catch (error :any) {
+      console.error(`Database connection error: ${error.message}`);
+      if (attempt < MAX_RETRIES) {
+        console.log(`Retrying in ${RETRY_DELAY / 1000} seconds...`);
+        await new Promise((resolve) => setTimeout(resolve, RETRY_DELAY));
+      } else {
+        console.error(
+          "Max retries reached. Unable to connect to the database."
+        );
+      }
     }
-   });
+  }
+}
 
-const router = Router();
+(async () => {
+  try {
+    await connectToDatabase();
 
-router.use("/auth", authRoutes);
-router.use("/users", userRoutes);
-router.use("/accounts", accountRoutes);
-router.use("/transactions", transactionRoutes);
-router.use("/payment-methods", paymentMethodRoutes);
+    const router = Router();
+    router.use("/auth", authRoutes);
+    router.use("/users", userRoutes);
+    router.use("/accounts", accountRoutes);
+    router.use("/transactions", transactionRoutes);
+    router.use("/payment-methods", paymentMethodRoutes);
 
-app.use("/api", router);
-
+    app.use("/api", router);
+  }
+  catch {
+    return;
+  }
+})();
 
 export default app;
